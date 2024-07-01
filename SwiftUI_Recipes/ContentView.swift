cf177d680e961a94d0ade2ae9c35706f36239f9c
Rebaz Salahaddin <ostan.jaff@gmail.com>
2024-07-01 17:13:20 +0300

    Initial Commit

---
+++ SwiftUI_Recipes/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI_Recipes
+//
+//  Created by Rebaz Salahaddin on 7/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI_Recipes/SwiftUI_RecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_RecipesApp.swift
+//  SwiftUI_Recipes
+//
+//  Created by Rebaz Salahaddin on 7/1/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_RecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
